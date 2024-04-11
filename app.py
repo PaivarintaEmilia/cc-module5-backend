bc4aebc8f8de80a90df521f1d48e7a7d65d75a81
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-04-11 12:31:23 +0200

    Testataan webhookin toimintaa.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     # Code here
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World Changes!</p>"
 
 
 if __name__ == "__main__":
