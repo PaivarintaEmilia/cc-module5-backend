92a1d8e162d2f7734f5081927f9f8af0e1b29d82
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-04-22 12:51:48 +0200

    22.4. Rahdin virhe ok. Muokataan koodia sopivaksi front-endin yhdistämiselle.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
@@ -7,42 +7,62 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
+from flask import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World Changes!</p>"
 
-df = pd.read_csv("train.csv", encoding="ISO-8859-1", engine="python", on_bad_lines="skip")
-#df.info()
-df.isnull().sum()
-df.dropna(inplace=True)
-df.isnull().sum()
 
-X = df['text']
-y = df['sentiment']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+@app.route("/analyze", methods=["POST"])
+def sentimental_analysis():
 
-text_clf = Pipeline([
-    ('tfidf', TfidfVectorizer()),
-    ('clf', LinearSVC())
-])
+    # Userinput
+    user_input = request.json['text']
 
-text_clf.fit(X_train, y_train)
+    # Starting
+    df = pd.read_csv("train.csv", encoding="ISO-8859-1", engine="python", on_bad_lines="skip")
+    #df.info()
 
-predictions = text_clf.predict(X_test)
-#print(classification_report(y_test, predictions))
+    # Delete the null values if needed
+    df.isnull().sum()
 
-cm = confusion_matrix(y_test, predictions)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=text_clf.classes_)
-disp.plot()
-#plt.show()
+    # Clean the data
+    df.dropna(inplace=True)
+    df.isnull().sum()
 
-# Making predictions and printing result
-#userInput = input("Insert your sentence here:\n")
-prediction = text_clf.predict(['I love dogs.'])
-print(f"Prediction: {prediction[0]}")
+    # Preparing data for training
+    X = df['text']
+    y = df['sentiment']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    # Creating and training the model
+    text_clf = Pipeline([
+        ('tfidf', TfidfVectorizer()),
+        ('clf', LinearSVC())
+    ])
+
+    text_clf.fit(X_train, y_train)
+
+    # Evaluating the data
+    predictions = text_clf.predict(X_test)
+    #print(classification_report(y_test, predictions))
+
+    # Visualizing the data (disabled for now)
+    cm = confusion_matrix(y_test, predictions)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=text_clf.classes_)
+    disp.plot()
+    #plt.show()
+
+    # Making predictions and printing result
+    #userInput = input("Insert your sentence here:\n")
+    prediction = text_clf.predict([user_input])
+    return jsonify({"result": prediction[0]})
+    print(f"Prediction: {prediction[0]}")
 
 
 
